2ddd230d44a205d4e290176f01f9d9580a5625b8
msantam2 <msantam2@gmail.com>
2018-09-09 19:10:44 -0700

    Add timestamp columns to media types table; run migrations up to local development DB

--- db/migrate/20180910012636_create_media_types.rb
+++ db/migrate/20180910012636_create_media_types.rb
@@ -2,6 +2,8 @@ class CreateMediaTypes < ActiveRecord::Migration[5.2]
   def change
     create_table :media_types do |t|
       t.string :name, null: false
+
+      t.timestamps
     end
 
     Api::MediaType.create(name: "movie")
