73a7f96aa1150afeab3dd12a21a7439b8a2050c2
msantam2 <msantam2@gmail.com>
2018-09-10 15:26:21 -0700

    Finish implementation of seeds file to populate movies & tv shows in the Postgres db

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 # gem 'rack-cors'
 
 gem 'rest-client', '~> 2.0', '>= 2.0.2'
+gem 'colorize', '~> 0.8.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,45 @@
-# GENRES
-raw_genres = RestClient.get("#{ENV['THE_MOVIE_DB_GENRES_URL']}", params: { api_key: "#{ENV['THE_MOVIE_DB_API_KEY']}" })
+# MOVIE GENRES
+puts "Fetching movie genres..."
+
+raw_genres = RestClient.get("#{ENV['THE_MOVIE_DB_MOVIE_GENRES_URL']}", params: { api_key: "#{ENV['THE_MOVIE_DB_API_KEY']}" })
 
 raw_genres = JSON.parse(raw_genres)
-
-genres_ref = {}
+movie_genres_ref = {}
 
 raw_genres["genres"].each do |genre|
   id = genre["id"]
   name = genre["name"]
-
-  genres_ref[id] = name  
+  movie_genres_ref[id] = name  
 end
+
+puts "Done fetching movie genres.".colorize(:green)
+puts
+
+
+
+# SHOW GENRES
+puts "Fetching TV show genres..."
+
+raw_genres = RestClient.get("#{ENV['THE_MOVIE_DB_SHOW_GENRES_URL']}", params: { api_key: "#{ENV['THE_MOVIE_DB_API_KEY']}" })
+
+raw_genres = JSON.parse(raw_genres)
+show_genres_ref = {}
+
+raw_genres["genres"].each do |genre|
+  id = genre["id"]
+  name = genre["name"]
+  show_genres_ref[id] = name  
+end
+
+puts "Done fetching TV show genres.".colorize(:green)
+puts
 
 
 
 # MOVIES
-page_count = ENV['THE_MOVIE_DB_PAGE_COUNT'].to_i
+puts "Fetching the movies!..."
 
+page_count = ENV['THE_MOVIE_DB_PAGE_COUNT'].to_i
 movies = []
 
 # note: the maximum page size is 20
@@ -27,6 +50,9 @@ page_count.times do |i|
   movies += raw_movies["results"]
 end
 
+puts "Done fetching movies.".colorize(:green)
+puts
+
 movies = movies.map do |movie|
   title = movie["title"]
   release_year = movie["release_date"].split("-").first.to_i
@@ -34,7 +60,7 @@ movies = movies.map do |movie|
 
   genres = []
   genre_ids = movie["genre_ids"]
-  genre_ids.each { |genre_id| genres << genres_ref[genre_id] }
+  genre_ids.each { |genre_id| genres << movie_genres_ref[genre_id] }
   genres = genres.join(", ")
 
   Api::Movie.new(title: title,
@@ -43,16 +69,61 @@ movies = movies.map do |movie|
                  genres: genres)
 end
 
+puts "Saving movie instances to your DB..."
+
 persisted_movie_count = 0
 movies.each { |movie| persisted_movie_count += 1 if movie.save }
-puts "#{persisted_movie_count} movies have been persisted to your database."
+puts "#{persisted_movie_count} movies have been persisted to your database.".colorize(:green)
 
 
 
 # SLEEP (in order to not exceed external API's requests/second quota)
-# sleep(10)
+puts
+puts "taking a quick break! be back in 10 (seconds)...".colorize(:blue)
+puts
+sleep(10)
 
 
 
 # SHOWS
+puts "Fetching the TV shows!..."
 
+page_count = ENV['THE_MOVIE_DB_PAGE_COUNT'].to_i
+shows = []
+
+# note: the maximum page size is 20
+page_count.times do |i|
+  raw_shows = RestClient.get("#{ENV['THE_MOVIE_DB_SHOWS_URL']}", params: { api_key: "#{ENV['THE_MOVIE_DB_API_KEY']}", first_air_date_year: "#{ENV['THE_MOVIE_DB_SHOWS_RELEASE_YEAR']}", page: (i + 1) })
+
+  raw_shows = JSON.parse(raw_shows)
+  shows += raw_shows["results"]
+end
+
+puts "Done fetching TV shows.".colorize(:green)
+puts
+
+shows = shows.map do |show|
+  title = show["name"]
+  release_year = show["first_air_date"].split("-").first.to_i
+  synopsis = show["overview"]
+
+  genres = []
+  genre_ids = show["genre_ids"]
+  genre_ids.each { |genre_id| genres << show_genres_ref[genre_id] }
+  genres = genres.join(", ")
+
+  Api::Show.new(title: title,
+                release_year: release_year,
+                synopsis: synopsis,
+                genres: genres)
+end
+
+puts "Saving TV show instances to your DB..."
+
+persisted_show_count = 0
+shows.each { |show| persisted_show_count += 1 if show.save }
+puts "#{persisted_show_count} shows have been persisted to your database.".colorize(:green)
+
+puts
+
+puts "Done!".colorize(:green)
