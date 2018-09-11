94a676e377098be2802ccf7d8e521e32dfa3ac2e
msantam2 <msantam2@gmail.com>
2018-09-10 17:00:37 -0700

    Implement GET tv show (#show) endpoint, with active model serializers gem to de-clutter JSON serialization for response

---
+++ app/serializers/api/show_serializer.rb
@@ -0,0 +1,10 @@
+module Api
+  class ShowSerializer < ActiveModel::Serializer
+    attributes :id, :title, :release_year, :synopsis, :genres, :media_type
+
+    def media_type
+      id = object.media_type_id
+      Api::MediaType.find_by_id(id).name
+    end
+  end
+end
